4c0e5235ef4e302342b854ca31ef4e867d97ae33
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-12-22 04:27:17 -0500

    pkl fix2

--- kickstarter/model.py
+++ kickstarter/model.py
@@ -5,9 +5,9 @@ import pickle
 # Placeholders to initialize model
 # category = 'Fashion'
 # currency = 'USD'
-# goal = 100
+# goal = 1000
 # month = 'july'
-# backers= 1000
+# backers= 10
 # campaign_length = 20
 # campaign_name_length = 20
 
@@ -17,7 +17,7 @@ import pickle
 #            'campaign_length':[campaign_length],'name_char_length':[campaign_name_length]})
 
 def model(input_data):
-    rf = pickle.load(open('rf_model_pickle_2.pkl', 'rb'))
+    rf = pickle.load(open('./rf_model_pickle_2.pkl', 'rb'))
     pred = rf.predict(input_data)
     return pred
 
